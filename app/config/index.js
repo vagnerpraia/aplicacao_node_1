8ccd52e22c412a6e7bd3ab7c1cf2f5cb702e6638
vagnerpraia <vagnerpraia@gmail.com>
2018-02-16 00:18:32 -0200

    Alterando start da aplicação

---
+++ app/config/index.js
@@ -0,0 +1,6 @@
+require('./database')
+
+const server = require('./server')
+const routes = require('./routes')
+
+routes(server)
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const server = require('./config/server')
-const database = require('./config/database')
-const routes = require('./config/routes')
-
-routes(server)
